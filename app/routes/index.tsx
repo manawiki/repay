7ff477b7600262ea6be86ce6fd579ff62a64f6bb
Mo Sattler <hi@mosattler.com>
2023-02-13 13:06:37 +0100

    improve page placeholder

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,24 +14,15 @@ export default function Index() {
   const { userCount } = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to RePay</h1>
       <ul>
         <li>
           <a
             target="_blank"
-            href="https://remix.run/tutorials/blog"
+            href="/admin"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
+            Admin Interface
           </a>
         </li>
         <li>
@@ -40,8 +31,8 @@ export default function Index() {
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Amount of users: {userCount}
+          <a target="_blank" href="https://payloadcms.com/docs" rel="noreferrer">
+            Payload Docs
           </a>
         </li>
       </ul>
