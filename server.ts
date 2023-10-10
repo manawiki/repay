8b02da00b80f901f07dbbe6a5807d2441850b018
xHomu <84349818+xHomu@users.noreply.github.com>
2023-10-10 15:00:49 -0700

    Implement Payload 2 (#9)
    We'll use the default adapters first.

--- server.ts
+++ server.ts
@@ -33,12 +33,10 @@ async function start() {
   const app = express();
 
   invariant(process.env.PAYLOAD_SECRET, "PAYLOAD_SECRET is required");
-  invariant(process.env.MONGODB_URI, "MONGODB_URI is required");
 
   // Initialize Payload
   await payload.init({
     secret: process.env.PAYLOAD_SECRET,
-    mongoURL: process.env.MONGODB_URI,
     express: app,
     onInit: () => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
