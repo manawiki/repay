ee92abdd186ac25c0a7787ef550bce05dc7a9c73
xHomu <84349818+xHomu@users.noreply.github.com>
2023-10-10 13:55:19 -0700

    Implement payload2 with vite bundler

--- payload.config.ts
+++ payload.config.ts
@@ -1,3 +1,6 @@
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { viteBundler } from "@payloadcms/bundler-vite";
 import { buildConfig } from "payload/config";
 import path from "path";
 import Users from "./cms/collections/Users";
@@ -6,9 +9,14 @@ export default buildConfig({
   serverURL: "http://localhost:3000",
   admin: {
     user: Users.slug,
+    bundler: viteBundler(),
   },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URI ?? false,
+  }),
   collections: [Users],
   typescript: {
     outputFile: path.resolve(__dirname, "cms/payload-types.ts"),
-  }
+  },
 });
