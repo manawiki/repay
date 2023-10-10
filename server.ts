d9ddd8ae76229e912cd43f47922b4970b0d105a3
xHomu <84349818+xHomu@users.noreply.github.com>
2023-10-10 14:52:14 -0700

    vite isn't observing tsconfig extension properly

--- server.ts
+++ server.ts
@@ -38,7 +38,7 @@ async function start() {
   // Initialize Payload
   await payload.init({
     secret: process.env.PAYLOAD_SECRET,
-    mongoURL: process.env.MONGODB_URI,
+    // mongoURL: process.env.MONGODB_URI,
     express: app,
     onInit: () => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
