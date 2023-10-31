f61060fe7120cfd37b3c5d1b3fbc1bce0b3caef4
xHomu <84349818+xHomu@users.noreply.github.com>
2023-10-31 15:27:34 -0700

    update lexical

--- payload.config.ts
+++ payload.config.ts
@@ -1,5 +1,5 @@
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
-import { slateEditor } from "@payloadcms/richtext-slate";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { viteBundler } from "@payloadcms/bundler-vite";
 import { buildConfig } from "payload/config";
 import path from "path";
@@ -18,7 +18,7 @@ export default buildConfig({
       },
     }),
   },
-  editor: slateEditor({}),
+  editor: lexicalEditor({}),
   db: mongooseAdapter({
     url: process.env.MONGODB_URI ?? false,
   }),
